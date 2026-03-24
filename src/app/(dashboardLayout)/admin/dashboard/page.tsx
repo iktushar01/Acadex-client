3497d7fa4d0efac90d4a51fc613a28205035ec97
iktushar01 <iktushar2020@gmail.com>
2026-03-24 09:51:11 +0600

    feat: Establish initial Next.js application structure with root, common, and dashboard layouts, including authentication, home, and dashboard pages.

---
+++ src/app/(dashboardLayout)/admin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const AdminDashboardPage = () => {
+    return (
+        <div>
+            admin dashboard page
+        </div>
+    );
+};
+
+export default AdminDashboardPage;
---
+++ src/app/(dashboardLayout)/layout.tsx
@@ -0,0 +1,14 @@
+const DashboardLayout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <>
+            dashboard layout
+            {children}
+        </>
+    );
+};
+
+export default DashboardLayout;
